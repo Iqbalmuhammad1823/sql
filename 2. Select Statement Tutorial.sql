b25dd13f264aba788fa6188d840c2ce40c889429
Muhammad Iqbal <iqbal.muhammad1823@gmail.com>
2025-04-14 10:28:48 +0700

    Add files via upload
    Learn basic sql from Alex the Analyst - Select and From statement tutorial

---
+++ 2. Select Statement Tutorial.sql
@@ -0,0 +1,14 @@
+SELECT *
+
+FROM parks_and_recreation.employee_demographics;
+
+SELECT first_name,
+last_name,
+age,
+(age + 10) *10 + 10
+FROM parks_and_recreation.employee_demographics;
+#PEMDAS
+
+select distinct age,
+gender
+from parks_and_recreation.employee_demographics;
